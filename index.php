118a8b167c659755ae132c8026dad15fb23a2758
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-03 16:16:25 +0600

    split the theme into many part

--- index.php
+++ index.php
@@ -1,110 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php get_header(); ?>
+<?php get_template_part("hero"); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-    <div class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h3 class="tagline">
-                        <?php bloginfo("description"); ?>
-                    </h3>
-                    <h1 class="align-self-center display-1 text-center heading">
-                        <?php bloginfo("name"); ?>
-                    </h1>
+<div class="posts">
+    <?php
+    while (have_posts()) {
+        the_post();
+    ?>
+        <div class="post" <?php post_class(); ?>>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="post-title">
+                            <!-- set post title/heading -->
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </h2>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="posts">
-        <?php
-        while (have_posts()) {
-            the_post();
-        ?>
-            <div class="post" <?php post_class(); ?>> 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h2 class="post-title">
-                                <!-- set post title/heading -->
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_title(); ?>
-                                </a>
-                            </h2>
-                        </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <p>
+                            <strong>
+                                <!-- set post author -->
+                                <?php the_author(); ?>
+                            </strong>
+                            <br />
+                            <!-- set post date -->
+                            <?php echo get_the_date(); ?>
+                        </p>
+
+                        <!-- set post tag(s) -->
+                        <?php echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>'); ?>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-4">
-                            <p>
-                                <strong>
-                                    <!-- set post author -->
-                                    <?php the_author(); ?>
-                                </strong>
-                                <br />
-                                <!-- set post date -->
-                                <?php echo get_the_date(); ?>
-                            </p>
-
-                            <!-- set post tag(s) -->
-                            <?php echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>'); ?>
-                        </div>
-
-                        <div class="col-md-8">
-                            <p>
-                                <!-- set post featured image -->
-                                <?php
-                                if (has_post_thumbnail()) {
-                                    the_post_thumbnail('large', array("class" => "img-fluid"));
-                                }
-                                ?>
-                            </p>
-
-                            <!-- set post content/paragraph -->
+                    <div class="col-md-8">
+                        <p>
+                            <!-- set post featured image -->
                             <?php
-                                the_excerpt();
+                            if (has_post_thumbnail()) {
+                                the_post_thumbnail('large', array("class" => "img-fluid"));
+                            }
                             ?>
+                        </p>
 
-                        </div>
+                        <!-- set post content/paragraph -->
+                        <?php
+                        the_excerpt();
+                        ?>
+
                     </div>
                 </div>
             </div>
-        <?php
-        }
-        ?>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 offset-md-5">
-                    <?php
-                    the_posts_navigation(array(
-                        "prev_text" => "New Posts",
-                        "next_text" => "Old Posts"
-                    ));
-                    ?>
-                </div>
-            </div>
         </div>
+    <?php
+    }
+    ?>
 
-    </div>
-
-    <div class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    &copy; LWHH - All Rights Reserved
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-5">
+                <?php
+                the_posts_navigation(array(
+                    "prev_text" => "New Posts",
+                    "next_text" => "Old Posts"
+                ));
+                ?>
             </div>
         </div>
     </div>
 
-    <?php wp_footer(); ?>
-</body>
+</div>
 
-</html>
+<?php get_footer(); ?>
