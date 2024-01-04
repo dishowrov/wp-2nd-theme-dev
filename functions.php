bf64dfc9d7a17ba74ea6645c5547470c147d48af
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-04 11:22:35 +0600

    added a widget

--- functions.php
+++ functions.php
@@ -14,3 +14,20 @@ function webly_assets()
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 add_action("wp_enqueue_scripts", "webly_assets");
+
+
+function webly_sidebar()
+{
+    register_sidebar(
+        array(
+            'name' => __('Sigle Post Sidebar', 'webly'),
+            'id' => 'sidebar-1',
+            'description' => __('It is a dummy, fake, faul, ajaira as well as vondo sidebar.', 'webly'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+        )
+    );
+}
+add_action('widgets_init', 'webly_sidebar');
--- single.php
+++ single.php
@@ -1,68 +1,89 @@
 <?php get_header(); ?>
 <?php get_template_part("hero"); ?>
 
-<div class="posts">
-    <div class="post" <?php post_class(); ?>>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="post-title text-center">
-                        <!-- set post title/heading -->
-                        <?php the_title(); ?>
-                    </h2>
+<div class="container-fluid">
+    <div class="container">
+        <div class="row">
+
+            <!-- contents part -->
+            <div class="col-md-8">
+                <div class="posts">
+                    <div class="post" <?php post_class(); ?>>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2 class="post-title text-center">
+                                        <!-- set post title/heading -->
+                                        <?php the_title(); ?>
+                                    </h2>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-9 text-center">
+                                    <p>
+                                        <strong>
+                                            <!-- set post author -->
+                                            <?php the_author(); ?>
+                                        </strong>
+                                        <br />
+                                        <!-- set post date -->
+                                        <?php echo get_the_date(); ?>
+                                    </p>
+
+                                    <!-- set post tag(s) -->
+                                    <?php echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>'); ?>
+                                </div>
+
+                                <div class="col-md-9 text-center">
+                                    <p>
+                                        <!-- set post featured image -->
+                                        <?php
+                                        if (has_post_thumbnail()) {
+                                            the_post_thumbnail('large', array("class" => "img-fluid"));
+                                        }
+                                        ?>
+                                    </p>
+
+                                    <!-- set post content/paragraph -->
+                                    <?php
+                                    the_content();
+                                    ?>
+
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-5 offset-md-3">
+                                    <!-- if comments open show the sec otherwise ignore it -->
+                                    <?php
+                                    if (comments_open()) :
+                                        comments_template();
+                                    endif;
+
+                                    next_post_link();
+                                    "<br/>";
+                                    previous_post_link();
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-9 text-center">
-                    <p>
-                        <strong>
-                            <!-- set post author -->
-                            <?php the_author(); ?>
-                        </strong>
-                        <br />
-                        <!-- set post date -->
-                        <?php echo get_the_date(); ?>
-                    </p>
-
-                    <!-- set post tag(s) -->
-                    <?php echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>'); ?>
-                </div>
-
-                <div class="col-md-9 text-center">
-                    <p>
-                        <!-- set post featured image -->
-                        <?php
-                        if (has_post_thumbnail()) {
-                            the_post_thumbnail('large', array("class" => "img-fluid"));
-                        }
-                        ?>
-                    </p>
-
-                    <!-- set post content/paragraph -->
-                    <?php
-                    the_content();
-                    ?>
-
-                </div>
+            <!-- sidebar part -->
+            <div class="col-md-4">
+                <?php
+                if (is_active_sidebar('sidebar-1')) {
+                    dynamic_sidebar('sidebar-1');
+                }
+                ?>
             </div>
 
-            <div class="row">
-                <div class="col-md-5 offset-md-3">
-                    <!-- if comments open show the sec otherwise ignore it -->
-                    <?php
-                    if (comments_open()) :
-                        comments_template();
-                    endif;
-
-                    next_post_link();
-                    "<br/>";
-                    previous_post_link();
-                    ?>
-                </div>
-            </div>
         </div>
     </div>
 </div>
+
 
 <?php get_footer(); ?>
