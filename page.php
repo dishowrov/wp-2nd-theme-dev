4543b9b9c6e95f200a95c3048ca2482324e8389e
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-17 10:28:21 +0600

    page template system

--- page.php
+++ page.php
@@ -1,85 +1,97 @@
 <?php get_header(); ?>
 <?php get_template_part("hero"); ?>
 
-<div class="posts">
-    <?php
-    while (have_posts()) {
-        the_post();
-    ?>
-        <div class="post" <?php post_class(); ?>>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="post-title">
-                            <!-- set post title/heading -->
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <p>
-                            <strong>
-                                <!-- set post author -->
-                                <?php the_author(); ?>
-                            </strong>
-                            <br />
-                            <!-- set post date -->
-                            <?php echo get_the_date(); ?>
-                        </p>
-
-                        <!-- set post tag(s) -->
-                        <?php echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>'); ?>
-                    </div>
-
-                    <div class="col-md-8">
-                        <p>
-                            <!-- set post featured image -->
-                            <?php
-                            if (has_post_thumbnail()) {
-
-                                the_post_thumbnail('large', array("class" => "img-fluid"));
-                            }
-                            ?>
-                        </p>
-
-                        <!-- set post content/paragraph -->
-                        <?php
-                        // if (!post_password_required()) {
-                        //     the_excerpt();
-                        // } else {
-                        //     echo get_the_password_form();
-                        // }
-
-                        the_excerpt();
-                        ?>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php
-    }
-    ?>
-
-
-    <!-- post navigation -->
+<div class="container-fluid">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 offset-md-5">
+
+            <!-- contents part -->
+            <div class="col-md-8">
+                <div class="posts">
+                    <div class="post" <?php post_class(); ?>>
+                        <div class="container">
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2 class="post-title">
+                                        <!-- set post title/heading -->
+                                        <?php the_title(); ?>
+                                    </h2>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-9 text-right">
+                                    <p>
+                                        <strong>
+                                            <!-- set post author -->
+                                            <?php the_author(); ?>
+                                        </strong>
+                                        <br />
+                                        <!-- set post date -->
+                                        <?php echo get_the_date(); ?>
+                                    </p>
+
+                                    <!-- set post tag(s) -->
+                                    <?php echo get_the_tag_list('<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>'); ?>
+                                </div>
+
+                                <div class="col-md-9 text-center">
+                                    <p>
+                                        <!-- set post featured image -->
+                                        <?php
+                                        if (has_post_thumbnail()) {
+                                            $thumbnail_url = get_the_post_thumbnail_url(null, "large");
+                                            
+                                            echo '<a href="'.$thumbnail_url.'" data-featherlight="image" class="popup-img">';
+                                            
+                                            the_post_thumbnail('large', array("class" => "img-fluid"));
+
+                                            echo "</a>";
+                                        }
+                                        ?>
+                                    </p>
+
+                                    <!-- set post content/paragraph -->
+                                    <?php
+                                    the_content();
+                                    ?>
+
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <!-- if comments open show the sec otherwise ignore it -->
+                                    <?php
+                                    if (comments_open()) :
+                                        comments_template();
+                                    endif;
+
+                                    next_post_link();
+                                    "<br/>";
+                                    previous_post_link();
+                                    ?>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- sidebar part -->
+            <div class="col-md-4">
                 <?php
-                the_posts_navigation(array(
-                    "prev_text" => "New Posts",
-                    "next_text" => "Old Posts"
-                ));
+                if (is_active_sidebar('sidebar-1')) {
+                    dynamic_sidebar('sidebar-1');
+                }
                 ?>
             </div>
+
         </div>
     </div>
-
 </div>
+
 
 <?php get_footer(); ?>
